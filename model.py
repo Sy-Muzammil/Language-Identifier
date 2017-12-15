57f9830698d9ec605b711cf58e0ab7f9c093f3de
Sy-Muzammil <syedmuzi31@gmail.com>
2017-12-15 09:45:59 +0530

    model

--- model.py
+++ model.py
@@ -2,12 +2,15 @@
 from __future__ import print_function
 import sys
 import os
+import enchant
 import numpy as np
-import gensim                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-from sets import Set
-d = str(5)
+from gensim.models.word2vec import Word2Vec
+d = 5
+en = enchant.Dict("en_US")
 #BASE_PATH = "/home/muzammil/Desktop/Summer_Project/"
-BASE_PATH = "/home/muzammil/Desktop/Summer_Project/"
+#BASE_PATH = "/home/muzammil/Desktop/Summer_Project/"
+
+BASE_PATH = "/DATA/USERS/irshad/Muzammil/Training_data/"
 eng = np.random.rand(64).astype(np.float32)
 tel = np.random.rand(64).astype(np.float32)
 ne = np.random.rand(64).astype(np.float32)
@@ -16,35 +19,29 @@ dummy = np.random.rand(64).astype(np.float32)
 
 
 
-model = gensim.models.KeyedVectors.load_word2vec_format(
-    BASE_PATH + "Training_data/ENG_ALL_ISCTOK_BNC_maskUN_C2SHFL_d64_m20_i1.vec",binary=True,limit=10000)
+model = Word2Vec.load_word2vec_format(
+    BASE_PATH + "ENG_ALL_ISCTOK_BNC_maskUN_C2SHFL_d64_m20_i1.vec",binary=True,limit=500000)
 
 
-
-
-import sys
 from itertools import cycle, izip
 
-import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import classification_report as cr
 np.random.seed(42)
-
-
 class MLPClassifier():
     """Multi Layered Perceptron with one hidden layer"""
 
-    def __init__(self, n_hidden=50, learning_rate=0.1, SGD=False):
+    def __init__(self, n_hidden=66, learning_rate=0.1, SGD=False):
         self.n_hidden = n_hidden
         self.learning_rate = learning_rate
         self.SGD = SGD
 
     def softmax(self, x):
-        """softmax normalization"""
+        """softma/x normalization"""
         np.exp(x, x)
         x /= np.sum(x, axis=1)[:, np.newaxis]
 
-    def fit(self, X, y, max_epochs=10):
+    def fit(self, X, y, max_epochs=100):
         # uniform labels
         print ("Training is started")
         self.lb = LabelBinarizer()
@@ -64,7 +61,6 @@ class MLPClassifier():
         self.bias1_ = np.zeros(self.n_hidden)
         self.weights2_ = np.random.uniform(-1/nH, 1/nH, size=(self.n_hidden, self.n_outs))
         self.bias2_ = np.zeros(self.n_outs)
-
         if self.SGD:
             # NOTE Stochastic Gradient Descent
             # initialize hidden-layer and output layer matrices 
@@ -73,13 +69,15 @@ class MLPClassifier():
             x_output = np.empty((1, self.n_outs))
             delta_o = np.empty((1, self.n_outs))
 
+            nrange = range(n_samples)
             for it in xrange(1, max_epochs+1):
-                for j in xrange(n_samples):
+                np.random.shuffle(nrange)
+                for j in nrange:
                     self._forward(X[j, None], x_hidden, x_output)
                     self._backward(X[j, None], y[j, None], x_hidden, x_output, delta_o, delta_h)
                 pred = self.predict(xtest)
                 #print("p:",pred)
-                #print("1: ",cr(ytest, pred))
+                print("1: ",cr(ytest, pred))
 
         else:
             # NOTE Gradient Descent
@@ -95,10 +93,9 @@ class MLPClassifier():
                 self._backward(X, y, x_hidden, x_output, delta_o, delta_h)
                 pred = self.predict(X)
                 print("2: ",cr(y1, pred))
-
     def sigmoid(self,x):
             return 1/(1+np.exp(-x/1))
-        
+
 
     # predict test patterns
     def predict(self, X):
@@ -126,15 +123,21 @@ class MLPClassifier():
 
         # calculate derivative of output layer
         delta_o[:] = y - x_output
-        delta_h[:] = np.dot(delta_o, self.weights2_.T) 
+        delta_h[:] = np.dot(delta_o, self.weights2_.T)
 
         # update weights
         self.weights2_ += self.learning_rate * np.dot(x_hidden.T, delta_o)
         self.bias2_ += self.learning_rate * np.mean(delta_o, axis=0)
         self.weights1_ += self.learning_rate * np.dot(X.T, delta_h)
         self.bias1_ += self.learning_rate * np.mean(delta_h, axis=0)
+up=lo=ti=[-0.25,0.25]
+othe = [0.25,-0.25]
 
+ilo,iup,urls,other = [-0.25,-0.25,-0.25,0.25],[-0.25,-0.25,0.25,-0.25],[-0.25,0.25,-0.25,-0.25],[0.25,-0.25,-0.25,-0.25]
 
+l1,l2,l3,l4 = [-0.25,-0.25,-0.25,0.25],[-0.25,-0.25,0.25,-0.25],[-0.25,0.25,-0.25,-0.25],[0.25,-0.25,-0.25,-0.25]
+
+an,al,nu,oth=[-0.25,-0.25,-0.25,0.25],[-0.25,-0.25,0.25,-0.25],[-0.25,0.25,-0.25,-0.25],[0.25,-0.25,-0.25,-0.25]
 
 
 class Build_W2V:
@@ -149,107 +152,97 @@ class Build_W2V:
 
 
     def generate_ngrams(self):
-        
+
         for sent in self.words:
             for (x,y,z) in zip(sent[0:-1],sent[1:-1],sent[2:]):
                 self.trigrams_words.append([x,y,z])
 
-        
+
         for sent in self.tags:
             for (x,y,z) in zip(sent[0:-1],sent[1:-1],sent[2:]):
                 self.trigrams_tags.append([x,y,z])
-        
-        self.use_embed()
+
+
+
+        # print trigrams_words
+        # print "-------------------------------------------------------------------------------"
+        # print trigrams_tags
+        # print "-------------------------------------------------------------------------------"
         self.use_embedding()
-    
-    def use_embed(self):
-        wrd = self.trigrams_words
-        tg = self.trigrams_tags
-
-        self.prefix1 = set()
-        self.sufix1 = set()
-        self.prefix2 = set()
-        self.sufix2 = set()
-
-        for i in range(len(wrd)):
-            #print(wrd[i])
-            for j in range(len(wrd[i])):
-                #print(temp)
-                if j == 0:
-                    temp = wrd[i][j]
-                    print(temp)
-                    pre = temp[:3]
-                    suf = temp[-3:]
-                
-                    self.prefix1.add(pre)
-                    self.sufix1.add(suf)
-                if j == 2:
-                    temp = wrd[i][j]
-                    pre = temp[:3]
-                    suf = temp[-3:]
-                
-                    self.prefix2.add(pre)
-                    self.sufix2.add(suf)
-
-
-        #print len(self.prefix)
-        #print len(self.sufix)
-        #print "----------------"
-        #print(len(self.prefix))
-        #print(len(self.sufix))
-        self.pre_dict1 = dict(zip(self.prefix1, range(len(self.prefix1)+1)))  #NOTE index 0 for OOV affixes
-        self.suf_dict1 = dict(zip(self.sufix1, range(len(self.sufix1)+1)))
-        self.pfx_embd1 = np.random.uniform(-0.5, 0.5, (len(self.prefix1)+1, 32))
-        self.sfx_embd1 = np.random.uniform(-0.5, 0.5, (len(self.sufix1)+1, 32))
-
-        self.pre_dict2 = dict(zip(self.prefix2, range(len(self.prefix2)+1)))  #NOTE index 0 for OOV affixes
-        self.suf_dict2 = dict(zip(self.sufix2, range(len(self.sufix2)+1)))
-        self.pfx_embd2 = np.random.uniform(-0.5, 0.5, (len(self.prefix2)+1, 32))
-        self.sfx_embd2 = np.random.uniform(-0.5, 0.5, (len(self.sufix2)+1, 32))
-
-        print(self.pfx_embd1.shape)
-        print(self.sfx_embd1.shape)
-        print(self.pfx_embd2.shape)
-        print(self.sfx_embd2.shape)
-
-        #print(self.predic)
-        #print("========================")
-        #print(self.sufdic)
-        #print("========================")
-
 
     def use_embedding(self):
 
+        #print self.trigrams_words
+        #print "-------------------------------"
+        #print self.trigrams_tags
+
         wrd = self.trigrams_words
         tg = self.trigrams_tags
+        #print wrd
+        #print tg
 
+        # for i in self.trigrams_words:
+        #   print i
         self.wordvec = []
         self.lisvec = []
+
         for i in range(len(wrd)):
             tempvec =[]
-            if wrd[i][1] in model:
-                tempvec.extend(model[wrd[i][1]])
-            elif wrd[i][1] not in model:
-                tempvec.extend(dummy)
+            #tempvec =np.array([])
+            #print ("1: ",self.wordvec.shape)
+            for j in range(len(wrd[i])):
+                if wrd[i][j] in model:
+                    tempvec.extend(model[wrd[i][j]])
+                else:
+                    tempvec.extend(dummy)
 
-            temp = wrd[i][0]
-            pre = temp[:3]
-            suf = temp[-3:]
-            x = self.pfx_embd1[self.pre_dict1.get(pre, 0)]
-            tempvec.extend(x)
-            x = self.sfx_embd1[self.suf_dict1.get(suf, 0)]
-            tempvec.extend(x)
+            #print("t: ",len(tempvec))
+            if en.check(wrd[i][1].lower()):
+                tempvec.extend(lo)
+            else:
+                tempvec.extend(othe)
+            if en.check(wrd[i][1].upper()):
+                tempvec.extend(up)
+            else:
+                tempvec.extend(othe)
+            if  en.check(wrd[i][1].title()):
+                tempvec.extend(ti)
+            else:
+                tempvec.extend(othe)
+
+             if wrd[i][1].islower():
+                tempvec.extend(ilo)
+            elif wrd[i][1].isupper():
+                tempvec.extend(iup)
+            elif wrd[i][1].startswith("http") or wrd[i][1].startswith("www.") or wrd[i][1].startswith("https") or wrd[i][1].startswith("@") or wrd[i][1].startswith("#"):
+                tempvec.extend(urls)
+            else:
+                tempvec.extend(other)
 
 
-            temp = wrd[i][2]
-            pre = temp[:3]
-            suf = temp[-3:]
-            x = self.pfx_embd2[self.pre_dict2.get(pre, 0)]
-            tempvec.extend(x)
-            x = self.sfx_embd2[self.suf_dict2.get(suf, 0)]
-            tempvec.extend(x)
+            if len(wrd[i][1]) < 3:
+                tempvec.extend(l1)
+            elif len(wrd[i][1]) > 2 and len(wrd[i][1]) <= 6:
+                tempvec.extend(l2)
+            elif len(wrd[i][1]) > 6 and len(wrd[i][1]) <= 10:
+                tempvec.extend(l3)
+            else:
+                tempvec.extend(l4)
 
+
+            if wrd[i][1].isalpha():
+                tempvec.extend(al)
+            elif wrd[i][1].isalnum():
+                tempvec.extend(an)
+            elif wrd[i][1].isdigit():
+                tempvec.extend(nu)
+            else:
+                tempvec.extend(oth)
+            #print "-----------"
+            #print tempvec
+            print("to: ",len(tempvec))
             tempvec = np.array(tempvec)
+
             self.wordvec.append(tempvec)
             if tg[i][1] == d:
                 self.lisvec.append(0)
@@ -260,32 +253,21 @@ class Build_W2V:
             elif tg[i][1] == "univ":
                 self.lisvec.append("univ")
             else:
-                self.lisvec.append("en")
-            #tempvec = np.array(tempvec.tolist())
-            #npa = np.asarray(tempvec, dtype=np.float32)
-            #arr = np.vstack(npa)
-            #print arr
-        #print"final------------->"
-        #print self.wordvec
-        #print" ========================================="
-        # print len(self.wordvec[0])
-        # print len(self.wordvec)
-        # print len(self.lisvec)
-        
-        #print "----------------"
+                self.lisvec.append("eng")
         self.wordvec = np.array(self.wordvec)
         self.lisvec = np.array(self.lisvec)
-        print(self.wordvec.shape)
-        print(self.lisvec.shape)
-    
+        print ("1: ",self.wordvec.shape)
+        print ("2: ",self.lisvec.shape)
+
+
 def process_data(file,words,tags):
     list1 = [] #word 
     list2 = [] #tag
-    
+
     i = 0
     with open(sys.argv[1],"r+") as fp:
         for line in fp:
-        
+
             if(i == 0):
                 list1.append(d)
                 list2.append(d)
@@ -302,16 +284,15 @@ def process_data(file,words,tags):
                 list1 = []
                 list2 = []
                 i = 0
-
 if __name__ == '__main__':
 
     words = []  #matrix of words each row containing sentence
     tags = []   # matrix of tags corresponding to each word
-    
+
     process_data(sys.argv[1],words,tags)
     W2V = Build_W2V(words,tags)
     #WV.TrainModel()
-    
+
     data_input = W2V.wordvec
     data_output = W2V.lisvec
     xtrain = data_input[:9000]
@@ -327,3 +308,5 @@ if __name__ == '__main__':
     #print("O: ",O)
     #print("pred: ",pred)
     print("0: ",cr(ytest, pred))
+                                                                                                                             319,1         Bot
+
